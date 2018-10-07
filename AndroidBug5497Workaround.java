bcdc7ab256ad8e6de09b76fd4fb4e8611530a389
William Crandell <william@crandell.ws>
2018-10-07 19:50:25 -0400

    ensure keyboard is open or closed before taking action...
    huge 2k devices use less screen space for keyboards make size calculation less reliable than on smaller devices so an additional check is add

--- AndroidBug5497Workaround.java
+++ AndroidBug5497Workaround.java
@@ -7,6 +7,7 @@ import android.view.WindowManager;
 import android.widget.FrameLayout;
 
 public class AndroidBug5497Workaround {
+    private static final String TAG = "aBug5497";
 
     // For more information, see https://issuetracker.google.com/issues/36911528
     // To use this class, simply invoke assistActivity() on an Activity that already has its content view set.
@@ -33,36 +34,68 @@ public class AndroidBug5497Workaround {
     }
 
     private void possiblyResizeChildOfContent() {
+        
+        
+        InputMethodManager imm = (InputMethodManager) activity
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        
         int usableHeightNow = computeUsableHeight();
         if (usableHeightNow != usableHeightPrevious) {
             int usableHeightSansKeyboard = mChildOfContent.getRootView().getHeight();
             int heightDifference = usableHeightSansKeyboard - usableHeightNow;
             if (heightDifference > (usableHeightSansKeyboard / 4)) {
 
-                // keyboard probably just became visible
-                frameLayoutParams.height = usableHeightSansKeyboard - heightDifference;
-
-                frameLayoutParams.topMargin = getStatusBarHeight();
-                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-                activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-
                 if(BuildConfig.DEBUG){
-                    Log.v("aBug5497", "keyboard probably just became visible");
+                    // keyboard probably just became visible
+                    Log.v(TAG, "keyboard probably just became visible");
+                }
+                
+                if (imm.isAcceptingText()) { 
+                    
+                    // keyboard is shown
+                    if(BuildConfig.DEBUG){
+                        Log.d(TAG,"Software keyboard is open");
+                    }
+                    frameLayoutParams.height = usableHeightSansKeyboard - heightDifference;
+                    frameLayoutParams.topMargin = getStatusBarHeight();
+                    
+                    activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                    activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+                    
+                } else {
+                    // keyboard is hidden
+                    if(BuildConfig.DEBUG) {
+                        Log.d(TAG, "Software Keyboard is closed");
+                    }
+
                 }
             } else {
 
-                // keyboard probably just became hidden
-                if(usableHeightPrevious != 0) {
-                    frameLayoutParams.height = usableHeightSansKeyboard;
-                    activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-                    activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+                // keyboard probably just became hidden
+                if (BuildConfig.DEBUG) {
+                    Log.v(TAG, "keyboard probably just became hidden");
                 }
 
-                frameLayoutParams.topMargin = 0;
+                if (imm.isAcceptingText()) {
+                    // keyboard is shown
+                    if (BuildConfig.DEBUG) {
+                        Log.d(TAG, "Software Keyboard is open");
+                    }
+                } else {
+                    // keyboard is hidden
+                    if(usableHeightPrevious != 0) {
+                        frameLayoutParams.height = usableHeightSansKeyboard;
+                        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+                        activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-                if(BuildConfig.DEBUG){
-                    Log.v("aBug5497", "keyboard probably just became hidden");
+                    }
+
+                    frameLayoutParams.topMargin = 0;
+
+                    if(BuildConfig.DEBUG) {
+                        Log.d(TAG, "Software Keyboard is closed");
+                    }
                 }
             }
 
@@ -70,11 +103,11 @@ public class AndroidBug5497Workaround {
             usableHeightPrevious = usableHeightNow;
 
             if(BuildConfig.DEBUG){
-                Log.v("aBug5497", "frameLayoutParams.height="+frameLayoutParams.height);
-                Log.v("aBug5497", "mChildOfContent.getTop()="+mChildOfContent.getTop());
-                Log.v("aBug5497", "mChildOfContent.getBottom()="+mChildOfContent.getBottom());
-                Log.v("aBug5497", "frameLayoutParams.topMargin="+frameLayoutParams.topMargin);
-                Log.v("aBug5497", "frameLayoutParams.bottomMargin="+frameLayoutParams.bottomMargin);
+                Log.v(TAG, "frameLayoutParams.height="+frameLayoutParams.height);
+                Log.v(TAG, "mChildOfContent.getTop()="+mChildOfContent.getTop());
+                Log.v(TAG, "mChildOfContent.getBottom()="+mChildOfContent.getBottom());
+                Log.v(TAG, "frameLayoutParams.topMargin="+frameLayoutParams.topMargin);
+                Log.v(TAG, "frameLayoutParams.bottomMargin="+frameLayoutParams.bottomMargin);
             }
         }
     }
